a8fedc22368d8a0d94b7d489e8e542d9cd1a101a
Vincent Fiorentini <vincent@panatechcomputer.com>
2014-10-19 15:24:22 -0700

    Updated the export functionality to match our file conventions.

--- PipeDreamLevelCreator/PDViewController.m
+++ PipeDreamLevelCreator/PDViewController.m
@@ -57,24 +57,59 @@
     [self.gridModel setValueAtRow:rowInt column:colInt to:currentValue];
 }
 
+// exportButtonPressed NSLogs a one-line string in this format:
+//<width> <height> <startXCoord> <startYCoord> <goalXCoord> <goalYCoord> <NExW for each cell ...>
 - (IBAction)exportButtonPressed {
+    // Note: these four strings are totally dependent on PDUtilities. (This is bad, but meh.)
+    NSString *startTxtString = @"computerHealthy";
+    NSString *goalTxtString = @"goal";
+    NSString *startOpeningsString = @"xExx";
+    NSString *goalOpeningsString = @"NESW";
+    
     NSInteger numRows = [self.gridModel numRows];
     NSInteger numCols = [self.gridModel numCols];
     
     NSMutableString *fileString = [[NSMutableString alloc] init];
     
+    //"<width> <height> "
+    [fileString appendString:[NSString stringWithFormat:@"%li %li ", numRows, numCols]];
     
-    [fileString appendString:[NSString stringWithFormat:@"\nRows: %li\n", numRows]];
-    [fileString appendString:[NSString stringWithFormat:@"Cols: %li\n", numCols]];
+    // find the start row/col and goal row/col
+    NSInteger startRow = -1;
+    NSInteger startCol = -1;
+    NSInteger goalRow = -1;
+    NSInteger goalCol = -1;
     for (int row = 0; row < numRows; row++) {
         for (int col = 0; col < numCols; col++) {
             NSInteger value = [self.gridModel getValueAtRow:row column:col];
             NSString *stringForValue = [PDUtilities txtForInt:value];
-            [fileString appendString:[NSString stringWithFormat:@"%@", stringForValue]];
+            if ([stringForValue isEqualToString:startTxtString]) {
+                startRow = row;
+                startCol = col;
+            } else if ([stringForValue isEqualToString:goalTxtString]) {
+                goalRow = row;
+                goalCol = col;
+            }
         }
-        [fileString appendString:@"\n"];
+    }
+    //"<startXCoord> <startYCoord> <goalXCoord> <goalYCoord>"
+    [fileString appendString:[NSString stringWithFormat:@"%li %li %li %li", startRow, startCol, goalRow, goalCol]];
+
+    //" <NExW>" for each cell ...
+    for (int row = 0; row < numRows; row++) {
+        for (int col = 0; col < numCols; col++) {
+            NSInteger value = [self.gridModel getValueAtRow:row column:col];
+            NSString *stringForValue = [PDUtilities txtForInt:value];
+            if ([stringForValue isEqualToString:startTxtString]) {
+                stringForValue = startOpeningsString;
+            } else if ([stringForValue isEqualToString:goalTxtString]) {
+                stringForValue = goalOpeningsString;
+            }
+            [fileString appendString:[NSString stringWithFormat:@" %@", stringForValue]];
+        }
     }
     
+    // NSLog the resulting level
     NSLog(@"%@", fileString);
 }
 
